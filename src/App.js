0bcda065eb64df7290f70df4877faa943849274e
Hatameh Talebi <talebi.ha@tiddev.com>
2020-11-02 11:06:18 +0330

    add contract component to menu

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { ThemeProvider } from '@tatareact/core/ThemeProvider';
 import { Switch, Route, BrowserRouter } from 'react-router-dom';
 import Page1 from './pages/page1';
 import Page2 from './pages/page2';
+import Page4 from './pages/page4';
 import Grid from "@material-ui/core/Grid/Grid";
 
 const TestModal = () => {
@@ -19,12 +20,13 @@ function App() {
               <BrowserRouter>
                   <Grid container spacing={0}>
                       <Grid item lg={12} md={12} sm={12} xs={12}>
-                           <ChqMenu items={[{title:'page1',url:'/page1'},{title:'page2',url:'/page2'}]}/>
+                           <ChqMenu items={[{title:'page1',url:'/page1'},{title:'page2',url:'/page2'},{title:'contract',url:'/page4'}]}/>
                       </Grid>
-                      <Grid item item lg={12} md={12} sm={12} xs={12} style={{padding:'5px'}}>
+                      <Grid item lg={12} md={12} sm={12} xs={12} style={{padding:'5px'}}>
                           <Switch>
                               <Route path="/page1" component={Page1} />
                               <Route path="/page2" component={Page2} />
+                              <Route path="/page4" component={Page4} />
                           </Switch>
                       </Grid>
                   </Grid>
