05fd59abc041fb1c8f6f2776f161145f7c75bee1
Hatameh Talebi <talebi.ha@tiddev.com>
2020-11-04 11:18:52 +0330

    change component url name

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import {Grid} from '@tatareact/core/Grid';
 import Page1 from './pages/page1';
 import Page2 from './pages/page2';
 import TabControl from './pages/TabControl';
+import SearchContract from './pages/searchContract';
 import Page3 from './pages/page3';
 
 
@@ -17,7 +18,7 @@ function App() {
               <BrowserRouter>
                   <Grid container spacing={0}>
                       <Grid item lg={12} md={12} sm={12} xs={12}>
-                           <ChqMenu items={[{title:'page1',url:'/page1'},{title:'page2',url:'/page2'},{title:'page3',url:'/page3'},{title:'TabControl',url:'/TabControl'}]}/>
+                           <ChqMenu items={[{title:'page1',url:'/page1'},{title:'page2',url:'/page2'},{title:'page3',url:'/page3'},{title:'TabControl',url:'/TabControl'},{title:'searchContract',url:'/searchContract'}]}/>
                       </Grid>
                       <Grid item lg={12} md={12} sm={12} xs={12} style={{padding:'5px'}}>
                           <Switch>
@@ -25,6 +26,7 @@ function App() {
                               <Route path="/page2" component={Page2} />
                               <Route path="/page3" component={Page3} />
                               <Route path="/TabControl" component={TabControl} />
+                              <Route path="/searchContract" component={SearchContract} />
                           </Switch>
                       </Grid>
                   </Grid>
